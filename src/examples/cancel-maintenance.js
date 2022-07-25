2d2837a711c86410caf68a74987754ec1f1482b5
William Augusto <william.santos315@outlook.com>
2022-07-25 14:23:13 -0300

    feat: exemplo de cancelamento de manutencao

---
+++ src/examples/cancel-maintenance.js
@@ -0,0 +1,31 @@
+import { client } from '../core/client.js'
+import { getData, getFistItem } from '../core/utils.js'
+import { strictEqual } from 'node:assert'
+
+async function run () {
+  const payload = {
+    cancellationReason: 'Equipe do conselho de administradores decidiu cancelar tais atividades!'
+  }
+
+  const pendingMaintenance = await client.get('/maintenances', {
+    params: {
+      page: 1,
+      perPage: 1,
+      statusIn: 'PENDING'
+    }
+  }).then(getFistItem)
+
+  const canceledMaintenance = await client.post(`/maintenances/${pendingMaintenance.id}/actions/cancel`, payload)
+    .then(getData)
+
+  strictEqual(pendingMaintenance.id, canceledMaintenance.id)
+  strictEqual(canceledMaintenance.status, 'CANCELED')
+  strictEqual(canceledMaintenance.cancellationReason, payload.cancellationReason)
+}
+
+run()
+  .catch(err => {
+    const error = err.isAxiosError ? err.toJSON() : err
+    console.log('error: ', error)
+    process.exit(1)
+  })
