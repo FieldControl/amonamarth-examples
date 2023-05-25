85c3494f6c1804c8fd61e9d19de9e03f8b88550e
willaug <william.santos315@outlook.com>
2023-05-25 14:50:29 -0300

    chore: documentacao de busca de empresas

---
+++ src/examples/get-companies.js
@@ -0,0 +1,78 @@
+// Exemplo baseado na documentação disponível em https://amonamarth.fieldcontrol.com.br/docs
+// Todos os dados informados são apenas exemplos de testes e não representam a verdade
+
+import { client } from '../core/client.js'
+import { getData } from '../core/utils.js'
+
+async function run () {
+  // Obter empresas que contenham "Field Control" em seus nomes (não sensitivo)
+  // Apenas um item por página
+  // Página um ou primeira página
+  await client.get('/companies', {
+    params: {
+      page: 1,
+      perPage: 1,
+      nameContains: 'Field Control'
+    }
+  }).then(getData)
+
+  // Obter empresas que contenham "Field Control" em seus nomes (não sensitivo)
+  // Apenas dois itens por página
+  // Página um ou primeira página
+  // Da mais antiga para a mais recente
+  await client.get('/companies', {
+    params: {
+      page: 1,
+      perPage: 2,
+      nameContains: 'Field Control',
+      orderColumn: 'createdAt',
+      orderDirection: 'asc'
+    }
+  }).then(getData)
+
+  // Obter empresas que possuem o nome idêntico a "Resolv"
+  // Apenas um item por página
+  // Página um ou primeira página
+  await client.get('/companies', {
+    params: {
+      page: 1,
+      perPage: 1,
+      nameEq: 'Resolv'
+    }
+  }).then(getData)
+
+  // Apenas dois itens por página
+  // Página um ou primeira página
+  // Com o nome de Z a A
+  await client.get('/companies', {
+    params: {
+      page: 1,
+      perPage: 2,
+      orderColumn: 'name',
+      orderDirection: 'desc'
+    }
+  }).then(getData)
+
+  // Apenas dois itens por página
+  // Página um ou primeira página
+  // Mais distante para mais proxima
+  // Campo "distanceRelativeTo" deve ser uma string contendo a latitude e longitude, respectivamente ("latitude,longitude")
+  // Campo "distanceRelativeTo" só pode ser informado se o campo orderColumn for igual a "distance"
+  // Empresas sem coordenadas ficarão no final da lista
+  await client.get('/companies', {
+    params: {
+      page: 1,
+      perPage: 2,
+      orderColumn: 'distance',
+      orderDirection: 'desc',
+      distanceRelativeTo: '37.7749,-122.4194'
+    }
+  }).then(getData)
+}
+
+run()
+  .catch(err => {
+    const error = err.isAxiosError ? err.toJSON() : err
+    console.log('error: ', error)
+    process.exit(1)
+  })
